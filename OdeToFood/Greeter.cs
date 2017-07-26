2bd229abae834c106309ae9a0101d39d8e2081f2
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-26 14:10:33 -0500

    added Greeter class and IGreeter to handle greeting message, added SingletonService in Startup.cs to pass a Greeter whenever an IGreeter is called.

---
+++ OdeToFood/Greeter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood
+{
+
+    public interface IGreeter //describes the capabilities of every greeter
+    {
+        string GetGreeting(); //all can run the GetGreeting method
+    }
+
+
+    public class Greeter : IGreeter //class to implement the interface
+    {
+        public string GetGreeting()
+        {
+            return "Hola from the Greeter!"; //sets the return value of the GetGreeting method for this implementation of IGreeter
+        }
+    }
+}
--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -31,10 +31,15 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory,
+            IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -45,7 +50,8 @@ namespace OdeToFood
 
             app.Run(async (context) =>
             {
-                var message = Configuration["Greeting"];
+                //var message = Configuration["Greeting"]; 
+                var message = greeter.GetGreeting();
                 await context.Response.WriteAsync(message);
 
                 //await context.Response.WriteAsync("Hello world!!!!!!!!");
