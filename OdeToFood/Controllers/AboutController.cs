c475277a83b2e3ff84adcd1a52b8db08b16554a0
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-27 07:38:40 -0500

    added tokens to the attribute based routing on the AboutController

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -3,17 +3,28 @@
 
 namespace OdeToFood.Controllers
 {
-
-    [Route("about")] //first part of http request is /about/
+    [Route("[controller]/[action]")] //grabs name of the controller, so this will serve for the About controller, and then the name 
+    //of the action. The action names are defined by the methods below. 
+    
+    
+    //[Route("about")] //first part of http request is /about/
     public class AboutController
     {
-        [Route("")] //action in http request is blank
+        //[Route("[action]")] //uses the method name to match the action part of the http request. this will 
+        //serve for the /phone action on the http request. 
+        //using the [action] token allows the name of the Phone method to change without having to change the routing. It'll automatically
+        //pick up the name of the method. 
+        
+        //[Route("")] //action in http request is blank
         public string Phone() //responds to the phone action on the http request of the about page... /about/phone
         {
             return "1-555-555-5555";
         }
 
-        public string Address() // /about/address
+        //[Route("[action]")] // /about/address
+        
+        //[Route("address")] //about/address
+        public string Address() 
         {
             return "USA";
         }
