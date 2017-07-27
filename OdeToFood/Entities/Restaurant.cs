2b02ec082b516a78ae87ab392fb07bcb21f4c742
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-27 10:34:08 -0500

    fixed typo

--- OdeToFood/Entities/Restaurant.cs
+++ OdeToFood/Entities/Restaurant.cs
@@ -18,7 +18,7 @@ namespace OdeToFood.Entities
     {
         public int Id { get; set; }
 
-        [Required, MaxLength(80]
+        [Required, MaxLength(80)]
         [Display(Name ="Restaurant Name"] //on the html, don't display the text "Name" on the input, display "Restaurant Name"
         public string Name { get; set; }
         public CuisineType Cuisine { get; set; }
