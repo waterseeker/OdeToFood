c4d337c138028adf177d5ad31540cd796dfb393c
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-27 08:46:44 -0500

    brought in IGreeter service to the HomeController and set a private field for it.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -10,16 +10,20 @@ namespace OdeToFood.Controllers
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData) //give me an instance of the IRestaurantData called restaurantData
+        public HomeController(IRestaurantData restaurantData, IGreeter greeter) //give me an instance of the IRestaurantData called restaurantData
+             //and an instance of the IGreeter called greeter
         {
-            _restaurantData = restaurantData;
+            _restaurantData = restaurantData; //saves the restaurantdata so we can consume it elsewhere
+            _greeter = greeter; //saves the greeter so we can use it elsewhere
         }
 
         public IActionResult Index()
         {
             var model = new HomePageViewModel(); //sets the model to an instance of the HomePageViewModel
             model.Restaurants = _restaurantData.GetAll();
+            model.CurrentMessage = "";
             //var model = _restaurantData.GetAll();
             //var model = new Restaurant { Id = 1, Name = "The House of Kobe" }; //instantiate a restaurant with an id 1 and name The House of Kobe
 
