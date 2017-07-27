a7ba340d91cf9b8452571d4dbdd80d5c27594afa
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-27 08:09:19 -0500

    Added a directive to the index.cshtml and called properties of the model to dynamically generate page content.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace OdeToFood.Controllers
         {
             var model = new Restaurant { Id = 1, Name = "The House of Kobe" }; //instantiate a restaurant with an id 1 and name The House of Kobe
 
-            return View();
+            return View(model);
 
             //return new ObjectResult(model); //without setup params, this will return a serialized object in json. 
 
