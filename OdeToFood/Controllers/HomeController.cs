fde7af5746d51e398536116d60adf56159fd709f
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-27 07:58:03 -0500

    changed response to a View() in the HomeController.

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -12,7 +12,9 @@ namespace OdeToFood.Controllers
         {
             var model = new Restaurant { Id = 1, Name = "The House of Kobe" }; //instantiate a restaurant with an id 1 and name The House of Kobe
 
-            return new ObjectResult(model); //without setup params, this will return a serialized object in json. 
+            return View();
+
+            //return new ObjectResult(model); //without setup params, this will return a serialized object in json. 
 
             //return Content ("Hello, from the HomeController!");
         }
