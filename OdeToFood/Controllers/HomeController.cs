169b622724e3d99d2f6ab3b9786ecb3058ccfc01
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-27 08:43:54 -0500

    changed usings in the restaurant.data to reflect folder name change from models to entities and removed unnecessary usings

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OdeToFood.Models;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 /// <summary>
 /// Summary description for Class1
@@ -18,7 +18,9 @@ namespace OdeToFood.Controllers
 
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomePageViewModel(); //sets the model to an instance of the HomePageViewModel
+            model.Restaurants = _restaurantData.GetAll();
+            //var model = _restaurantData.GetAll();
             //var model = new Restaurant { Id = 1, Name = "The House of Kobe" }; //instantiate a restaurant with an id 1 and name The House of Kobe
 
             return View(model);
