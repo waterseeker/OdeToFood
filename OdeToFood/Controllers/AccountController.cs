5ead12714178095a4af0dd5dacfda3e8672b6d4b
Burris, Wayne <Wayne.Burris@protective.com>
2017-07-28 10:19:11 -0500

    added error messaging and user registration

--- OdeToFood/Controllers/AccountController.cs
+++ OdeToFood/Controllers/AccountController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Entities;
 using OdeToFood.ViewModels;
+using System.Threading.Tasks;
 
 namespace OdeToFood.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -12,9 +24,26 @@ namespace OdeToFood.Controllers
         }
         
         [HttpPost, ValidateAntiForgeryToken]
-        public IActionResult Register(RegisterUserViewModel model)
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
         {
-
+            if(ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username };
+                var createResult = await _userManager.CreateAsync(user, model.Password);
+                if (createResult.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach(var error in createResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
         }
     }
 }
